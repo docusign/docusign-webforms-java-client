7d6a650f6be75022b6d82c984d36e1f914548e2b
root <root@devcenteradmin.docusigntest.com>
2024-02-14 04:14:51 -0800

    Version 1.0.0-v1.1.0-1.0.2 release

--- src/main/java/com/docusign/webforms/client/ApiClient.java
+++ src/main/java/com/docusign/webforms/client/ApiClient.java
@@ -96,7 +96,7 @@ public class ApiClient {
     String javaVersion = System.getProperty("java.version");
 
     // Set default User-Agent.
-    setUserAgent("Swagger-Codegen/1.1.0/1.0.2-RC12/Java/" + javaVersion);
+    setUserAgent("Swagger-Codegen/1.1.0/1.0.0/Java/" + javaVersion);
 
     // Setup authentications (key: authentication name, value: authentication).
     authentications = new HashMap<String, Authentication>();
